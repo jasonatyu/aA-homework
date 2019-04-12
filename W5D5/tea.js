de8d2b6cecb70a7c3313f4e7bf26884d066cd9ba
Jason Yu <jason@jasonyu.co>
2019-04-11 19:28:23 -0700

    Add JS homework

---
+++ W5D5/tea.js
@@ -0,0 +1,40 @@
+// window.setTimeout(function() {
+//     alert('HAMMERTIME')
+// }, 5000);
+
+
+// function hammerTime(time) {
+//     window.setTimeout(function () {
+//         alert(`${time} is hammertime!`)
+//     }, 5000);
+// };
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function teaAndBiscuits() {
+
+    reader.question("Would you like some tea?", function (first) {
+        console.log(`${first}`);
+
+        reader.question("Would you like some biscuits?", function (second) {
+            console.log(`${second}`);
+
+            const firstRes = (first === 'yes') ? 'do' : 'don\'t';
+            const secondRes = (second === 'yes') ? 'do' : 'don\'t';
+            console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
+            reader.close();
+        });
+        
+    });
+}
+
+
+
+teaAndBiscuits();
+
+
